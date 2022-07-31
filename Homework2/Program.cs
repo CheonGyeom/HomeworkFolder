8f92813764e850292ade7b9be23ecb6ee32fd0d6
CheonGyeom <109446720+CheonGyeom@users.noreply.github.com>
2022-07-31 12:08:08 +0900

    Create Program.cs

---
+++ Homework2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            int i1 = 3;
+            string s1 = "Hi";
+
+            Console.WriteLine("{0} {1}" ,i1,s1);
+            Console.WriteLine($"{i1} {s1}");
+            Console.WriteLine(i1 + " " + s1);
+        }
+    }
+}
